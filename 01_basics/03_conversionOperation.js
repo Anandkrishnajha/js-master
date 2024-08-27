22c42ee18db3a42ab512a60256644ac893d6ac26
Anand Kumar Jha <116617214+Anandkrishnajha@users.noreply.github.com>
2024-08-27 23:59:06 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="anand"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//33-number
+//"33"-string
+//"33abc"-Nan
+//null-object
+//undefined-undefined
+
+
+let isLoggedIn="anand"
+let booleanLoggedIn=Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+//1-true
+//0-false
+//""-false
+//"anand"-true
+
