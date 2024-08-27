4b44a917c227e9721e9c067695c1428107297093
Anand Kumar Jha <116617214+Anandkrishnajha@users.noreply.github.com>
2024-08-27 08:07:52 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144552
+let accountEmail="anand07@goole.com"
+var accountPassword="12345"
+accountCity="Bengaluru"
+
+
+// accountId=2//not allowed
+
+console.log(accountId);
+accountEmail="akj07@akj.com"
+accountPassword="212121"
+accountCity="Jaipur"
+/*prefer not to use var
+because of isse in block scope and functional scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"//treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs,not browser
+
+console.log(3
+    +
+    3);//it will work, but don't write the code like this because code readability should be high
+
+console.log("Anand");
+
+let name="Anand"
+let age=22
+let isLoggedIn=false
+let state;
+
+//number => 2 to power of 53 is the range
+//bigint => if a number is not in range of number then we can use it
+//string =>" " for write any word
+//boolean=> true/false
+//null => standalone value
+//undefined => this the the value which is not defined
+//symbol => unique
+
+//object => it is type of null
+
+console.log(typeof name);//string
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
+
