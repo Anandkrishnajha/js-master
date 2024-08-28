8f6189a949be2187b8247f55749da0555381f33a
Anand Kumar Jha <116617214+Anandkrishnajha@users.noreply.github.com>
2024-08-28 00:35:50 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,67 @@ let booleanLoggedIn=Boolean(isLoggedIn)
 //""-false
 //"anand"-true
 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Anand"
+let str3=str1+str2
+// console.log(str3);
+
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log( (3 + 4) * 5 % 3);
+
+
+
+console.log(+true);
+console.log(-true);
+console.log(+false);
+console.log(-false);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+//============pre and post increment and decrement
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+ 
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
