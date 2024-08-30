40ac73da13bc6bae8862b9ba7c257aaea8ba96f6
Anand Kumar Jha <116617214+Anandkrishnajha@users.noreply.github.com>
2024-08-30 06:27:06 +0000

    if-else

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,66 @@
+//++++++++if statements
+
+// const isUserLoggedIn=true
+const temeperature=41
+
+// if(temeperature==40){
+//     console.log("less than 40");   
+// }else{
+//     console.log("temperature is highg");  
+// }
+
+//<,>,<=,>=,==,!=,===,!==
+
+// const score=20
+// if(score>100){
+//     let power="fly"
+//     console.log(`UserPower: ${power}`);
+    
+// }else{
+//     console.log("No power");
+    
+// }
+
+
+
+const balance=600;
+// if(balance>500)console.log("test"),console.log("test2");  //it will also give output but not good practice
+
+
+// if(balance<500){
+//     console.log("less than 500");   
+// }else if (balance<750){
+//     console.log("less than 750");
+    
+// }else{
+//     console.log("Less than 1200");
+    
+// }
+
+
+
+
+
+
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggesInFromEmail=true
+
+if(userLoggedIn && debitCard &&10==1){
+    console.log("Allow to purcahse course");
+    
+}
+   //here if && is used then all condition must satisfy
+
+
+
+if(loggedInFromGoogle||loggesInFromEmail){
+    console.log("user logged in");
+    
+}  //here in || or operator if any one condtion satisfy then it will execute
+
+/*
+
+*/
